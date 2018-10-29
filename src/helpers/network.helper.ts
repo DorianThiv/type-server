1eefad99f6d4762326110477bd1f7682ffeada7a
Dorian Thivolle <dorian.thivolle@inneasoft.com>
2018-10-29 16:25:35 +0100

    Found network helper find network interfaces.

---
+++ src/helpers/network.helper.ts
@@ -0,0 +1,30 @@
+
+import * as os from 'os';
+
+export class NetworkHelper {
+
+    public static ip() {
+        const ifaces = os.networkInterfaces();
+        Object.keys(ifaces).forEach(function (ifname) {
+            let alias = 0;
+            ifaces[ifname].forEach(function (iface) {
+                console.log(ifname);
+                console.log(iface);
+                if ('IPv4' !== iface.family || iface.internal !== false) {
+                    // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+                    return;
+                }
+            
+                if (alias >= 1) {
+                    // this single interface has multiple ipv4 addresses
+                    console.log(ifname + ':' + alias, iface.address);
+                } else {
+                    // this interface has only one ipv4 adress
+                    // console.log(ifname, iface.address);
+                }
+            ++alias;
+            });
+        });    
+    }
+
+} 
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { QuartzModuleLoader } from "./modules/quartz.loader";
+import { NetworkHelper } from "./helpers/network.helper";
 
 class Quartz {
 
     public static start() {
         console.log('Server quartz start');
-        QuartzModuleLoader.initialize();
-        QuartzModuleLoader.execute();
+        NetworkHelper.ip();
+        // QuartzModuleLoader.initialize();
+        // QuartzModuleLoader.execute();
         return 0;
     }
 
