cc0b0730232d64271d59717b06ae020a7cf1d753
Dorian Thivolle <dorian.thivolle@inneasoft.com>
2018-10-29 15:11:07 +0100

    Remove dist/ folder

--- dist/modules/interfaces/server-module.interface.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-/**
- * Server Modules Interface
- * ------------------------
- * Interface all modules to be loaded more efficiently
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=server-module.interface.js.map
